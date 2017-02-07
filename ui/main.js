ba2bd0b880ca278a1a3d9820eda242f786063e88
harsh makadia <harshmakadiaa@gmail.com>
2017-02-07 12:17:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,9 @@
 console.log("loaded!");
 
+var element = document.getElementById("text");
+element.innerHTML = "new value";
+
+var img = document.getElementsByTagName("img");
+img.onclick = function() {
+  img.style.width="100px";  
+};
