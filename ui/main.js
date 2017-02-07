1653e0aa9f4f80ac653e42cc889b387f820f4ca9
harsh makadia <harshmakadiaa@gmail.com>
2017-02-07 18:01:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var element = document.getElementById("text");
 element.innerHTML = "new value";
 
 var img = document.getElementById("img");
-var maginLeft = 0;
+var marginLeft = 0;
 function moveRight(){
     marginLeft = marginLeft+10;
     img.style.marginLeft = marginLeft + 'px';
