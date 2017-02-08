a1110f6514f6482575f9fde50b1cc00d3d0edc29
harsh makadia <harshmakadiaa@gmail.com>
2017-02-08 20:14:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,24 @@
-console.log("loaded!");
+var button = document.getElementById("counter");
 
-var element = document.getElementById("text");
-element.innerHTML = "new value";
-
-var img = document.getElementById("img");
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft+5;
-    img.style.marginLeft = marginLeft + 'px';
-}
-
-img.onclick = function() {
-  var interval = setInterval(moveRight, 50);
+button.onclick = function() {
+    var request = new XMLHttpRequest();  
+    
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE){
+            
+            if(request.status === 200){
+                
+            
+            var counter = request.responseText;
+            var span = document.getElementById("count");
+            span.innerHTML = counter.toString();
+            
+            }
+            
+        }
+    };
+    
+    request.open('GET', '', true);
+    request.send(null);
+    
 };
